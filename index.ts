e94c2e37f9b17dcec95a74278fe75b1763653b81
Sarah Fowler <sarah.fowler73@gmail.com>
2020-07-05 15:32:02 -0400

    Add test puzzles; add read file from cli

--- index.ts
+++ index.ts
@@ -11,7 +11,7 @@ import {
 
 const fs = require("fs");
 
-console.log(process.argv0);
+// Converts a string test file into a list of lists of values
 const processTestFile = (data: string) => {
   const splitRows = data.trim().split(/\s+/); // Split on \n, \r, or any other \s character
   const splitData = splitRows.map((row) =>
@@ -21,10 +21,10 @@ const processTestFile = (data: string) => {
   return splitData as Array<Array<Value>>;
 };
 
-fs.readFile(
-  "/users/sarahfowler/Downloads/Sudoku_test_puzzles/SudokuPuzzle1.txt",
-  "utf8",
-  (err, data) => {
+// Reads test file from path, processes it, runs through the two agent solvers
+// and prints the results
+const runAgents = (path: string) =>
+  fs.readFile(path, "utf8", (err, data) => {
     if (err) throw err;
     console.log(data);
     const puzzle = processTestFile(data);
@@ -36,5 +36,15 @@ fs.readFile(
       mrvAgent(genRootNode(puzzle), getAllBlankCoords(puzzle)),
       "mrv"
     );
-  }
-);
+  });
+
+// Read file path from command line
+const filePath = process.argv[2];
+
+if (typeof filePath !== "string") {
+  console.log("Error: Please pass the path to a test file\n");
+} else if (!fs.existsSync(filePath)) {
+  console.log(`Error: "${filePath}" does not exist. Please check your input\n`);
+} else {
+  runAgents(filePath);
+}
